1dfce7c92966d0d4e57d1067e031f4f8c02d8469
skorea6 <skorea6@naver.com>
2024-11-25 00:06:08 +0900

    데이터베이스 접속 URL 수정

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/twitter_test";
+    private static final String URL = "jdbc:mysql://localhost:3306/twitter";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
